b4d2f009b3170c1ff5234316ab128045183cc8bd
Lakshmimgowda <167877524+Lakshmimgowda@users.noreply.github.com>
2024-04-23 17:56:16 +0530

    Add files via upload

---
+++ sqlfile.sql
@@ -0,0 +1,49 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.1.54-community : Database - charm
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`charm` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `charm`;
+
+/*Table structure for table `filehistory` */
+
+DROP TABLE IF EXISTS `filehistory`;
+
+CREATE TABLE `filehistory` (
+  `filename` varchar(25) DEFAULT NULL,
+  `cloudallocated` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `filehistory` */
+
+insert  into `filehistory`(`filename`,`cloudallocated`) values ('datatosend.txt','1,2');
+
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `emailid` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `login` */
+
+insert  into `login`(`username`,`password`,`emailid`) values ('lakshmi','lakshmi','lakshmi@gmail.com');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
